91c1846aa230d81d0863ddcdd346da5b065c0183
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-10 17:07:09 +0100

    completed email

--- mail/js/script.js
+++ mail/js/script.js
@@ -4,9 +4,32 @@ console.log('JS OK!')
 /* 
 1 -Crea un array di email;
 2 -Chiedi all’utente la sua email.
-3 -controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+3 -controlla che sia nella lista di chi può accedere
+4 -stampa un messaggio appropriato in console sull’esito del controllo.
 */ 
 
 //1
 const authorizedUsers = ['pippo@gmail.com', 'pluto@yahoo.it','topolino@hotmail.com', 'paperino@outlook.com','amelia@vesuvio.net'];
 
+console.table(authorizedUsers);
+
+//2 
+
+const userEmail = prompt("Inserire un' email valida per l' accesso", 'pippo@gmail.com');
+
+console.log('userEmail:',userEmail);
+
+//3 ->confrontare l' email fornita dall' utente con l' email dell array
+
+let message = "l' email inserita non è valida";
+
+for (let i = 0; i < authorizedUsers.length; i++) {
+    
+    if (userEmail === authorizedUsers[i]) {
+        message = "l' email inserita è valida"
+    }    
+}
+
+//4
+
+console.log(message);
