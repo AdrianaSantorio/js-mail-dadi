762b6e564ff0b61a74e6460c7251d6b11e559ea3
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-10 16:03:17 +0100

    initialized repository

---
+++ dadi/js/script.js
@@ -0,0 +1,13 @@
+
+// # Gioco dei dadi
+/*
+ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampare in console i tiri di dado e il risultato.
+*/
+
+/*
+1- generare un numero random per l' utente e il cpu
+2- comparare i due numeri e stabilire quale sia il più alto
+3- Stampare in console i tiri di dado e il risultato
+*/
